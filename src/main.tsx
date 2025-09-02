ef661a0cc35ff669b747d3639c7f64e00967ae85
Yanik dos Santos <yrsvntos@gmail.com>
2025-09-02 23:14:31 +0100

    Configura CartProvider e Toaster no main.tsx para contexto e notificações

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,18 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './App'
+import { CartProvider } from './context/CartContextAPI'
+import { Toaster } from 'react-hot-toast'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <CartProvider>
+      <Toaster
+        position="bottom-center"
+        reverseOrder={false}
+        
+      />
+      <RouterProvider router={router} />
+    </CartProvider>
   </StrictMode>,
 )
